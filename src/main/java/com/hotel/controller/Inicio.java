2bc8a7a6949187aea2cd7952f8cc8f4d9ab5158b
kmorasoto <98502739+kmorasoto@users.noreply.github.com>
2022-08-24 03:49:13 -0600

    Finalización de i18N

--- src/main/java/com/hotel/controller/Inicio.java
+++ src/main/java/com/hotel/controller/Inicio.java
@@ -22,4 +22,69 @@ public class Inicio {
     public String nosotros(){
     return "/nosotros";
     } 
+    
+    @GetMapping ("/detallesRooms")
+    public String detallesRooms(){
+    return "/detallesRooms";
+    } 
+    
+     @GetMapping ("/eventos")
+    public String eventos(){
+    return "/eventos";
+    }
+    
+    @GetMapping ("/habitaciones")
+    public String habitaciones(){
+    return "/habitaciones";
+    }
+      
+    @GetMapping ("/logins")
+    public String logins(){
+    return "/logins";
+    }
+    
+    @GetMapping ("/masDetallesR1")
+    public String masDetallesR1(){
+    return "/masDetallesR1";
+    }
+    
+     @GetMapping ("/masDetallesR2")
+    public String masDetallesR2(){
+    return "/masDetallesR2";
+    }
+    
+     @GetMapping ("/masDetallesR3")
+    public String masDetallesR3(){
+    return "/masDetallesR3";
+    }
+    
+     @GetMapping ("/masDetallesR4")
+    public String masDetallesR4(){
+    return "/masDetallesR4";
+    }
+        
+     @GetMapping ("/preguntasFrecuentes")
+    public String preguntasFrecuentes(){
+    return "/preguntasFrecuentes";
+    }
+    
+    @GetMapping ("/registro")
+    public String registro(){
+    return "/registro";
+    }
+    
+    @GetMapping ("/restauranteQuattro")
+    public String restauranteQuattro(){
+    return "/restauranteQuattro";
+    }
+    
+     @GetMapping ("/restauranteSakura")
+    public String restauranteSakura(){
+    return "/restauranteSakura";
+    }
+    
+     @GetMapping ("/toursS")
+    public String toursS(){
+    return "/toursS";
+    }
 }
